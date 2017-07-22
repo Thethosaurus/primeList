93f1e579a7b997d883eb40c564f774bec94b7539
Thethosaurus <jorgebarrios547@gmail.com>
2017-07-21 21:22:04 -0500

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,20 +1,3 @@
-# def countPrimes(n):
-#     # for i in range(2, n+1):
-#     #     for j in range(2, n // 2):
-#     #         if i % j == 0:
-#     #             break
-#     #         else:
-#     #             print i
-#     for i in range(2, n-1):
-#         primeVerify = n/i
-#         if type(primeVerify)==int:
-#             if primeVerify>1:
-#                 print i
-#                 print "hello"
-# countPrimes(100);
-
-
-
 number = raw_input("List Upper Bound?")
 def isPrime(n):
     for i in range(n, n-1):
@@ -33,7 +16,5 @@ def primeList(n):
         else:
             print i
             primeArray.append(i)
-            # isPrime(i);
-        # print "hello"
 
 primeList(int(number));
